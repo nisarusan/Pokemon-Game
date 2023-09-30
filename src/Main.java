0b2987aad1f461bee77de37f177cb1362645f7bf
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-09-30 04:17:55 +0200

    New

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    String name;
+    int test;
+    char tes;
+
+
+}
+
---
+++ src/Pokemon/pokemonGeneric.java
@@ -0,0 +1,50 @@
+import
+public class pokemonGeneric {
+
+//    name
+//            level
+//    hp (health points)
+//    xp (experience points)
+//    height
+//            weight
+//    food
+//            sound
+//    defence
+//            attack
+//    special
+//            accuracy
+
+//    Pokèmon kunnen ook ontzettend veel verschillende acties uitvoeren. Zo kun je denken aan de volgende methodes:
+//
+//    eats()
+//    speaks()
+//    thunderPunch()
+//    cometPunch()
+//    surf()
+//    leafStorm()
+//    inferno()
+//    electroBall()
+//    pyroBall*()
+//    solarBeam()
+//    hydroPump()
+//    thunder()
+//    hydroCanon()
+//    fireLash()
+//    leechSeed()
+//    voltTackle()
+//    rainDance()
+//    leaveBlade()
+//    flameThrower()
+//    pound()
+//    scratch()
+//    gust()
+//    hornAttack()
+//    psyBeam()
+
+
+    String getName;
+    String name;
+    int hp;
+
+    public static pokemonGeneric ()
+}
